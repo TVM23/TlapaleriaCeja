709576f45fc36b9e5059ce039488bc32d5fd9b62
TVM23 <tadeo23manzo@outlook.com>
2024-02-24 17:52:52 -0600

    ModeloBodega

---
+++ TlapaleriaCeja.Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TlapaleriaCeja.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre solo se compone de 60 caracteres como máximo ")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo descripcion es requerido")]
+        [MaxLength(100, ErrorMessage = "La descripcion solo se compone de 100 caracteres como maximo")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El estado de la bodega es requerido")]
+        public bool Estado { get; set; }
+    }
+}
